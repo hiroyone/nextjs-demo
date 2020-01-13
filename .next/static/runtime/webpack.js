f877fbd413842ddd4a0355e7eca7b82b4a494184
hiroyone <yonetani@team-lab.com>
2020-01-13 14:56:16 +0900

    Added dynamic routing

--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e768795cff9a1262e691";
+/******/ 	var hotCurrentHash = "3f25b78ac89000b23ee8";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,25 @@
-const Index = () => (
-    <div>
-        <p>Hello Next.js</p>
-    </div>
+import Layout from '../components/MyLayout';
+import Link from 'next/link';
 
+const PostLink = props => (
+    <li>
+        <Link href="/p/[id]" as={`/p/${props.id}`}>
+            <a>{props.id}</a>
+        </Link>
+    </li>
 );
 
-export default Index
+export default function Blog(){
+    return (
+        <Layout>
+            <h1>My Blog</h1>
+            <ul>
+                <PostLink id="hello-nextjs"/>
+                <PostLink id="learn-nextjs" />
+                <PostLink id="deploy-nextjs" />
+            </ul>
+        </Layout>
+    )
+}
+
+
